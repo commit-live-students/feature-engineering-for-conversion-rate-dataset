bdfeb29377ed52ea9c0b200232c18d9dd7582ba3
adinathauti <adinathauti@gmail.com>
2017-07-11 14:47:34 +0000

    Done

--- build.py
+++ build.py
@@ -1,29 +1,78 @@
-def csv_to_dataframe(filepath):
-    pass
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as pre
+import math
 
 
-def dtype_category(dataframe, column_list):
-    pass
+
+def csv_to_dataframe(pth):
+    try:
+        df = pd.read_csv(pth)
+        #print df.head(10)
+        return df
+    except IOError:
+        print "Supposed to raise a FileNotFoundError"
 
 
-def centre_and_scale(dataframe, column_list):
-    pass
+def dtype_category(df,ls):
+    for col in ls:
+        if col not in set(df.columns.values):
+            raise KeyError
+        df[col] = df[col].astype('category')
+    return df
 
 
-def label_encoder(dataframe, column_list):
-    pass
+def centre_and_scale(df,ls):
+    for col in ls:
+        if col not in set(df.columns.values):
+            raise KeyError
+        df[col] = pre.scale(df[col])
+    #print df.head(10)
+    return df
 
 
-def one_hot_encoder(dataframe, column_list):
-    pass
+def skewness(df,ls):
+    ret_ls = []
+    for col in ls:
+        if col not in set(df.columns.values):
+            raise KeyError
+        #print("Skew for column:{} is {}".format(col,df[col].skew()))
+        ret_ls.append(df[col].skew())
+    return ret_ls
 
 
-def skewness(dataframe, column_list):
-    pass
+def sqrt_transform(df,ls):
+    ret_ls = []
+    cols_ls = []
+    for col in ls:
+        col_sqrt = col+'_sqrt'
+        if col not in set(df.columns.values):
+            raise KeyError
+        df[col_sqrt] = df[col].apply(math.sqrt)
+        cols_ls.append(df[col_sqrt])
+    return cols_ls
 
 
-def sqrt_transform(dataframe, column_list):
-    pass
+
+def label_encoder(df,ls):
+    le = pre.LabelEncoder()
+    for col in ls:
+        if col not in set(df.columns.values):
+            raise KeyError
+        le.fit(df[col])
+        df[col] = le.transform(df[col])
+    return df
+
+
+def one_hot_encoder(df,ls):
+    ohe = pre.OneHotEncoder()
+    for col in ls:
+        if col not in set(df.columns.values):
+            raise KeyError
+        df = pd.concat([df,pd.get_dummies(df[col])],axis=1)
+        #df[col] = ohe.transform(df[col])
+    return df
 
 
 def plots(dataframe, column_list):
